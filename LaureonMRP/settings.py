7f0ad0e88d0c1cb8657d6c97f941ed42f1cc250c
Dominique Ramirez-Garceau <dominiqueramirez-garceau@mac.mynetworksettings.com>
2025-08-06 23:16:44 -0400

    fixed settings.py

--- LaureonMRP/settings.py
+++ LaureonMRP/settings.py
@@ -13,13 +13,12 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import environ
 
-# Initialise environ
-env = environ.Env()
-environ.Env.read_env()
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Initialise environ
+env = environ.Env()
+environ.Env.read_env(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
